cf6fc6e87ef3c81ce11ba94200e911c3c339e61d
sqykly <sqykly@gmail.com>
2018-03-15 17:12:39 -0400

    Tests for fixes

--- valuenetwork/valueaccounting/tests/__init__.py
+++ valuenetwork/valueaccounting/tests/__init__.py
@@ -7,4 +7,5 @@ from valuenetwork.valueaccounting.tests.test_recipes import *
 from valuenetwork.valueaccounting.tests.test_event_saving import *
 from valuenetwork.valueaccounting.tests.test_stages import *
 from valuenetwork.valueaccounting.tests.test_value_equations import *
-
+from valuenetwork.valueaccounting.tests.test_transfers import *
+from valuenetwork.valueaccounting.tests.test_resource_type import *
