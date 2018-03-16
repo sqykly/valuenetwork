9106efeab5e8a72311f07155859dd7e7ce68326c
sqykly <sqykly@gmail.com>
2018-03-16 14:09:17 -0400

    tests for fix to #510
    Also de-commented import of orders tests to include them in __init__,py; not sure why they were commented, so if there are issues, try disabling the rest of the tests in test_orders.py

--- valuenetwork/valueaccounting/tests/test_orders.py
+++ valuenetwork/valueaccounting/tests/test_orders.py
@@ -22,9 +22,9 @@ class OrderTest(WebTest):
     """
 
     def setUp(self):
-        
+
         self.user = User.objects.create_user('alice', 'alice@whatever.com', 'password')
-        
+
         facets = Facets()
         self.electronic_pattern = facets.electronic_pattern
         self.full_pattern = facets.full_pattern
@@ -32,8 +32,8 @@ class OrderTest(WebTest):
         source_us = facets.source.values.get(value="Us")
         #
         atype = AgentType(
-            name='Network', 
-            party_type='network', 
+            name='Network',
+            party_type='network',
             is_context=True)
         atype.save()
         self.project = EconomicAgent(
@@ -45,7 +45,7 @@ class OrderTest(WebTest):
         self.parent = recipe.parent
         self.child = recipe.child
         self.grandchild = recipe.grandchild
-        
+
         self.wf_recipe = WorkFlowRecipe()
         self.changeable = self.wf_recipe.changeable
         self.another_changeable = self.wf_recipe.another_changeable
@@ -77,7 +77,8 @@ class OrderTest(WebTest):
         sale_pattern = ProcessPattern(
             name="Sale pattern",
         )
-        order_pattern.save()
+        #order_pattern.save() # huh?
+        sale_pattern.save()
 
         use_case = UseCase.objects.get(identifier="sale")
 
@@ -86,7 +87,7 @@ class OrderTest(WebTest):
             use_case=use_case,
         )
         puc.save()
-        
+
         event_type_sale = EventType(
             name="Sale",
             label="sells",
@@ -94,7 +95,7 @@ class OrderTest(WebTest):
             related_to="agent",
             resource_effect="=",
         )
-        event_type_sale.save()  
+        event_type_sale.save()
 
         pfv = PatternFacetValue(
             pattern=order_pattern,
@@ -102,32 +103,32 @@ class OrderTest(WebTest):
             event_type=event_type_sale,
         )
         pfv.save()
-        
+
         rtfv = ResourceTypeFacetValue(
             resource_type=self.parent,
             facet_value=sellable_sellable,
         )
         rtfv.save()
-        
+
         rtfv = ResourceTypeFacetValue(
             resource_type=self.changeable,
             facet_value=sellable_sellable,
         )
         rtfv.save()
-        
+
         rtfv = ResourceTypeFacetValue(
             resource_type=self.another_changeable,
             facet_value=sellable_sellable,
         )
         rtfv.save()
-        
+
     def test_create_order(self):
         """Test create_order view
 
             and subsequent dependent demand explosion
-            
+
         """
-       
+
         response = self.app.get('/accounting/create-order/' , user='alice')
         form = response.form
         #import pdb; pdb.set_trace()
@@ -145,14 +146,34 @@ class OrderTest(WebTest):
         grandchild_input = child_process.incoming_commitments()[0]
         self.assertEqual(grandchild_input.quantity, Decimal("18"))
 
-        
+
+    def test_order_validation(self):
+        """Test fix for #510 Create order blows up
+            at least one item must have quantity > 0
+        """
+        # probably don't need this, but it can't hurt
+        Commitment.objects.filter(resource_type=self.child).delete()
+        try:
+            response = self.app.get('/accounting/create-order/' , user='alice')
+            form = response.form
+            #import pdb; pdb.set_trace()
+            due_date = datetime.date.today().strftime('%Y-%m-%d')
+            form["due_date"] = due_date
+            form["RT-6-quantity"] = 0
+            response = form.submit("submit1").follow()
+        except Exception as e:
+            raise AssertionError((_('an empty order should not throw, but threw %s' % str(e)),))
+
+        coms = Commitment.objects.filter(resource_type=self.child)
+        self.assertFalse(coms)
+
     def test_create_workflow_order(self):
         """Test create_order for a workflow item
 
             and subsequent dependent demand explosion
-            
+
         """
-       
+
         response = self.app.get('/accounting/create-order/' , user='alice')
         form = response.form
         #import pdb; pdb.set_trace()
@@ -178,15 +199,15 @@ class OrderTest(WebTest):
         prevs = last_process.previous_processes()
         self.assertTrue(first_process in prevs)
         self.assertTrue(last_process in nexts)
-        
-           
+
+
     def test_two_workflow_item_order(self):
         """Test create_order for two workflow items
 
             and subsequent dependent demand explosion
-            
+
         """
-       
+
         response = self.app.get('/accounting/create-order/' , user='alice')
         form = response.form
         #import pdb; pdb.set_trace()
@@ -196,7 +217,7 @@ class OrderTest(WebTest):
         form["RT-10-quantity"] = 4000
         response = form.submit("submit1").follow()
         #import pdb; pdb.set_trace()
-        
+
         pcs = self.changeable.producing_commitments()
         count = pcs.count()
         self.assertEqual(count, 2)
@@ -211,7 +232,7 @@ class OrderTest(WebTest):
         prevs = last_process.previous_processes()
         self.assertTrue(first_process in prevs)
         self.assertTrue(last_process in nexts)
-        
+
         pcs = self.another_changeable.producing_commitments()
         count = pcs.count()
         self.assertEqual(count, 2)
@@ -226,11 +247,11 @@ class OrderTest(WebTest):
         prevs = last_process.previous_processes()
         self.assertTrue(first_process in prevs)
         self.assertTrue(last_process in nexts)
-        
+
         order = last_pc.order_item.order
         processes = order.all_processes()
         self.assertEqual(len(processes), 4)
-        
+
     def test_two_order_items_with_same_resource_type(self):
         due_date = datetime.date.today()
         order = Order(
@@ -276,7 +297,7 @@ class OrderTest(WebTest):
         chain = commitment2.process_chain()
         self.assertEqual(len(chain), 2)
         #import pdb; pdb.set_trace()
-        
+
     def test_create_order_item(self):
         due_date = datetime.date.today()
         order = Order(
@@ -291,10 +312,10 @@ class OrderTest(WebTest):
         )
         #import pdb; pdb.set_trace()
         self.assertEqual(order.order_items().count(), 1)
-        
+
     def test_create_order_item_using_inherited_recipe(self):
         due_date = datetime.date.today()
-        shipment_et = EventType.create('Shipment', 'ships', 'shipped by', 'shipment', 'exchange', '-', 'quantity')      
+        shipment_et = EventType.create('Shipment', 'ships', 'shipped by', 'shipment', 'exchange', '-', 'quantity')
         order = Order(
             order_type="rand",
             name="test",
@@ -316,10 +337,10 @@ class OrderTest(WebTest):
         rt = order.order_items()[0].resource_type
         self.assertEqual(rt, heir)
         self.assertEqual(len(order.all_processes()), 2)
-        
+
     def test_create_order_item_using_inherited_workflow_recipe(self):
         due_date = datetime.date.today()
-        shipment_et = EventType.create('Shipment', 'ships', 'shipped by', 'shipment', 'exchange', '-', 'quantity')      
+        shipment_et = EventType.create('Shipment', 'ships', 'shipped by', 'shipment', 'exchange', '-', 'quantity')
         order = Order(
             order_type="rand",
             name="test",
@@ -349,7 +370,7 @@ class OrderTest(WebTest):
         p = order.all_processes()[0]
         rt = p.output_resource_types()[0]
         self.assertEqual(rt, heir)
-        
+
     def create_3_process_order(self):
         #add another process to self.wf_recipe
         package_pt = ProcessType(
@@ -357,12 +378,12 @@ class OrderTest(WebTest):
             estimated_duration=7200,
         )
         package_pt.save()
-        
+
         to_be_event_type = self.wf_recipe.to_be_event_type
         change_event_type = self.wf_recipe.change_event_type
         change_pt = self.wf_recipe.change_pt
         unit = self.wf_recipe.unit
-        
+
         package_input = ProcessTypeResourceType(
             process_type=package_pt,
             resource_type=self.changeable,
@@ -372,7 +393,7 @@ class OrderTest(WebTest):
             unit_of_quantity=unit,
         )
         package_input.save()
-        
+
         package_output = ProcessTypeResourceType(
             process_type=package_pt,
             stage=package_pt,
@@ -382,7 +403,7 @@ class OrderTest(WebTest):
             unit_of_quantity=unit,
         )
         package_output.save()
-        
+
         due_date = datetime.date.today()
         order = Order(
             name="test",
@@ -406,12 +427,12 @@ class OrderTest(WebTest):
 
     def test_order_process_sequence(self):
         """ Test sequencing when processes have same dates.
-        
+
             This was a bug, test failed before code fix.
         """
         order = self.create_3_process_order()
         order_item = order.order_items()[0]
-        
+
         processes_b4 = order.all_processes()
         oi_processes_b4 = order_item.all_processes_in_my_order_item()
         #import pdb; pdb.set_trace()
@@ -424,7 +445,7 @@ class OrderTest(WebTest):
         oi_processes_after = order_item.all_processes_in_my_order_item()
         self.assertEqual(processes_b4, processes_after)
         self.assertEqual(oi_processes_b4, oi_processes_after)
-        
+
     def test_delete_last_order_process(self):
         order = self.create_3_process_order()
         order_item = order.order_items()[0]
@@ -437,7 +458,7 @@ class OrderTest(WebTest):
         self.assertEqual(len(processes), 2)
         new_last_process = order.last_process_in_order()
         self.assertEqual(new_last_process.name, "change")
-        
+
     def test_delete_middle_order_process(self):
         order = self.create_3_process_order()
         order_item = order.order_items()[0]
@@ -451,4 +472,3 @@ class OrderTest(WebTest):
         self.assertEqual(len(processes), 2)
         last_process = order.last_process_in_order()
         self.assertEqual(last_process.name, "package")
-        
